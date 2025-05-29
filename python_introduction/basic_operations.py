b421425bb5ed67ba73c5d1b79ba8cdb10747aeff
parksomin72 <fatimazahraealawi@gmail.com>
2025-05-28 18:40:43 -0700

    Simple Interest Calculator

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,8 @@
 number1 = 10
 number2 = 5
-print("Addition of", number1, "and", number2, "is", number1 + number2)
-print("Subtraction of", number1, "and", number2, "is", number1 - number2)
-print("Multiplication of", number1, "and", number2, "is", number1 * number2)
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
